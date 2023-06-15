0b67d247cb0cf580319bba47fc5095225f18cd4c
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-15 03:07:25 -0700

    prints out a text through a puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,9 @@
 
 int main(void)
 {
+/* constant character message prints the string*/
 	const char *message = "\"Programming is like building a multilingual puzzle";
-
+/* puts function is return upon calling it and returns message variable*/
 	puts(message);
 
 	return (0);
