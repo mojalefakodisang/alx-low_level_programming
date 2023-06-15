8ff40fce14db5275e41de30310c0680e8cf0e02a
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-15 03:50:40 -0700

    prints out a text through a puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * Main -prints string message under puts() function
- * Return: 0 (Success)
+ * main - Entry point
+ *  Return: Always return 0 (Succes)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
