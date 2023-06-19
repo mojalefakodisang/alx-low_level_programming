041d04381b15a405252d7cb6538f9d8d35c743b6
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-19 14:39:18 -0700

    prints all sigle digit numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * return: (0) on success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
