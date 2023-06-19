0567ba7561114c3ebb81523b7615a297cecfd211
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-19 12:51:56 -0700

    prints lower case alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,17 @@
  *
  * lowerCaseAlphabets: prints alphabets from a to z
  * Return: returns the fuction
- */
+*/
 
-int lowerCaselphabets(void)
+int main(void)
 {
-	char letters;
-	for (letters = 'a'; letters <= 'z'; letters++)
-	{
-		putchar(letters);
+	int c;
+	
+	c ='a';
+	while (c <= 'z') {
+		putchar(c);
+		c++;
 	}
+	putchar('\n');
 	return (0);
 }
