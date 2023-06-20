aab4a1dad0d52d10c57d8b127cdd36841cc5bacd
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-20 15:26:51 -0700

    prints alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabets 10 times
+ *
+ * Return: No value to return
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 10)
+	{
+		char letters;
+
+		letters = 'a';
+		while (letters <= 'z')
+		{
+			_putchar(letters);
+			letters++;
+		}
+		_putchar(i);
+		i++;
+		_putchar('\n');
+	}
+}
