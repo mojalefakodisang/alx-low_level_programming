a93180dd25ef579f9e2fcc3804179eb05d13734e
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-20 06:17:00 -0700

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * printPutchar - Prints putchar followed by a new line
+ *
+ * Return: No return value
+ */
+
+char printPutchar(void)
+{
+	printf("_putchar \n");
+	return (0);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printPutchar();
+	return (0);
+}
