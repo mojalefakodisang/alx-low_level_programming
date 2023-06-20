47f0d054a8c0311c419a6a6dcc3439f56036576c
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-20 08:04:00 -0700

    main and header file

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * printPutchar - Prints putchar followed by a new line
+ *
+ * Return: No return value
+ */
+
+char printPutchar(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "1-alphabet.c"
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char print_alphabet();
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
