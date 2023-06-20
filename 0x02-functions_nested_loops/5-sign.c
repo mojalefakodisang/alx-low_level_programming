d666e47e8fba00314914fbb414561bd72460375f
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-20 16:26:50 -0700

    prints signs of any given number c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of a given number @n
+ * @n - Any given number to be tested
+ *
+ * Return: Returns 1 on success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+	return (0);
+}
