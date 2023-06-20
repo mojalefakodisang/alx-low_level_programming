a03daad314953a2c40130949d9159a19adaa412b
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-20 09:12:38 -0700

    has main file of 2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check code for Holberton School students
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
