722412095519311a60b6c10fe94cd1be0ecc165d
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-20 06:49:19 -0700

    prints all alphabets in alphabetical order

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints all alphabets in alphabetical order
+ *
+ * Return: No return value
+ */
+char print_alphabet(void)
+{
+	char i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
