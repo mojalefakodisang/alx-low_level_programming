3a7ad8657199bbf1525e7aca1a770dee1467cc80
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-20 18:45:49 -0700

    Displays time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints time
+ * @H, @h, @M, @m: Displays time
+ *
+ * Return: No value to return
+ */
+void jack_bauer(void)
+{
+	int H, h, M, m;
+
+	for (H = 0; H <= 2; H++)
+	{
+		for (h = 0; h <= 3; h++)
+		{
+			if ((H <= 1 && h <= 9) || (H <= 2 && h <= 3))
+			{
+				for (M = 0; M <= 5; M++)
+				{
+					for (m = 0; m <= 9; m++)
+					{
+						_putchar(H + '0');
+						_putchar(h + '0');
+						_putchar(58);
+						_putchar(M + '0');
+						_putchar(m + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
