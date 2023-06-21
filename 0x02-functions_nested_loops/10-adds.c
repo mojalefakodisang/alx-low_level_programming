513194fd4e707ddeac5f0eb0f8d7bee2c825ae52
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-20 19:37:13 -0700

    adds two interger values

---
+++ 0x02-functions_nested_loops/10-adds.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Adds integers a and b
+ * @a: any interger value
+ * @b: Any interger value
+ *
+ * Return: Return sum of the values on success
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
