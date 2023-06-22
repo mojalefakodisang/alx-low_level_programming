fd28625c97cb5dfd136aa32beaee56f8c85d5a00
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-22 13:39:17 -0700

    prints out fizz or buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,20 +16,21 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("%s \n", c);
+			printf("%s ", c);
 		}
 		else if (i % 3 == 0)
 		{
-			printf("%s \n", a);
+			printf("%s ", a);
 		}
 		else if (i % 5 == 0)
 		{
-			printf("%s \n", b);
+			printf("%s ", b);
 		}
 		else
 		{
-			printf("%d \n", i);
+			printf("%d ", i);
 		}
 	}
+	printf("\n");
 	return (0);
 }
