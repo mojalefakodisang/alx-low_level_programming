8bd533c4f47377631421ec36d98e27490b1a1ecb
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-22 13:34:32 -0700

    prints out fizz or buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints Fizz or Buzz
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	char *a = "Fizz";
+	char *b = "Buzz";
+	char *c = "FizzBuzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("%s \n", c);
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s \n", a);
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s \n", b);
+		}
+		else
+		{
+			printf("%d \n", i);
+		}
+	}
+	return (0);
+}
