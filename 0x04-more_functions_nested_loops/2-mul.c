a6a906072d2fd9b154292b70587fb4b755a6742b
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-21 21:18:59 -0700

    returns product of two variables

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - the function multiplies variables a and b
+ * @a: first variable
+ * @b: second variable
+ *
+ * Return: Returns product of the two variables on success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
