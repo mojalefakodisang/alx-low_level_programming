7628c0b040f1e73ca90cf8eacea90ef0cd8b6d97
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-26 15:26:16 -0700

    Prints a string with its elements in multiples of 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - Prints elements of @str by its index in multiples of 2
+ * @str: String to be tested
+ * Return: No return value
+ */
+void puts2(char *str)
+{
+	int index;
+	int length;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	for (index = 0; index < (length - 1); index += 2)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
