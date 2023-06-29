0e1d8db12056718c95df9c6c03167f93ab5b9cb5
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-28 23:58:48 -0700

    Append src string to dest by n characters

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - Appends src string into dest string
+ * @dest: First string
+ * @src: String to be appended to dest
+ * Return: Returns dest string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0' && n > 0)
+	{
+		dest[a] = src[b];
+		a++;
+		n--;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
