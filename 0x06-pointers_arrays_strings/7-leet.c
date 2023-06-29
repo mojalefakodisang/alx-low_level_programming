2bbaefccd67843b0036bdabc9cc58ed174e4a913
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-29 02:33:07 -0700

    Converts certain letters to numbers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - Converts certain letters to numbers
+ * @s: String to be converted
+ * Return: Returns the string
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeEoOtT1L";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (s);
+}
