3de41def4abd6e5df8d014462a6abff56f1e2620
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-29 03:51:38 -0700

    Copies one string onto another

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _strncpy - copies src into dest string
+ * @dest: Destination string
+ * @src: Source string
+ * @n: String length
+ * Return: Returns dest string
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	i = 0;
 	while (src[i] != '\0' && i < n)
 	{
