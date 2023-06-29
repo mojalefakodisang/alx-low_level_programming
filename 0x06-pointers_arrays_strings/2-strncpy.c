13ce479ea14f711af9eda30381ab74b2912c2532
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-06-29 00:29:22 -0700

    copies characters fromsrc to dest until n

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
