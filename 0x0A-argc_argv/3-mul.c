9f48abe0478d24cc4f5b3f7c0bbca04eb53431f9
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-07 12:45:15 -0700

    multiplies arguments passed to the program

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,44 +1,5 @@
-#include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
-
-/**
- * _atoi - Converts characters into integers
- *
- * @s: string to be converted
- *
- * Return: Returns result on success, else 0
- */
-int _atoi(char *s)
-{
-	int i;
-	int sign;
-	int result;
-
-	i = 0;
-	sign = 1;
-	result = 0;
-
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i = 1;
-	}
-	while (s[i] != '\0')
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-		}
-		else
-		{
-			return (0);
-		}
-		i++;
-	}
-	result *= sign;
-	return (result);
-}
-
 /**
  * main - Multiplies arguments passed onto the program
  *
@@ -62,7 +23,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			mul *= _atoi(argv[i]);
+			mul *= atoi(argv[i]);
 		}
 	}
 	printf("%d\n", mul);
