087e3f38dae3086f790756f8455fed800773e339
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-07 13:12:18 -0700

    prints number of arguments passed on the program

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
  * main - Prints the number of arguments passed into the program
  *
  * @argc: number of arguments passed
- *
+ * @argv: Argument vector
  * Return: Always 0 on success
  */
 int main(int argc, char __attribute__ ((unused)) *argv[])
