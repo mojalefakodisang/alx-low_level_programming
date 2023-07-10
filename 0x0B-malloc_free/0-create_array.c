1e4bd1c1c21ff8b4b4144aacb04ad8d22da7bf1f
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-10 16:28:44 -0700

    creates an array of characters

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array of characters
+ *
+ * @size: size of the array
+ *
+ * @c: character to be printed
+ *
+ * Return: returns pointer to the array else NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	str = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	str[i] = '\0';
+	return (str);
+}
