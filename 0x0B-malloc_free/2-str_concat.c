529d46e762c31e72353a518e524c6930b288b398
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-10 18:39:22 -0700

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,24 +17,23 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	int len1, len2;
 
-	if (s1 == NULL)
+	if (s1 != NULL)
 	{
-		return (NULL);
+		len1 = 0;
+		while (s1[len1] != '\0')
+		{
+			len1++;
+		}
 	}
-	if (s2 == NULL)
+	if (s2 != NULL)
 	{
-		return (NULL);
+		len2 = 0;
+		while (s2[len2] != '\0')
+		{
+			len2++;
+		}
 	}
-	len1 = 0;
-	while (s1[len1] != '\0')
-	{
-		len1++;
-	}
-	len2 = 0;
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
+
 	result = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 	if (result == NULL)
 	{
