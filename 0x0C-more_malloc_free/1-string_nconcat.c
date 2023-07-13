6758e29a4e32962053f21f6cf8410c4156fa4cc8
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-13 11:56:03 -0700

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,9 +42,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	while (i < len1)
+	while (i = 0; i < len1; i++)
 	{
-		ptr[i++] = s1[i++];
+		ptr[i] = s1[i];
 	}
 	while (n < len2 && i < (len1 + n))
 	{
