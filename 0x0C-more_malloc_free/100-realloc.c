897c5f9e08bf44c1d44eb0423053f8800c2260d1
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-13 15:38:14 -0700

    reallocates memory into a new memory area size

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,65 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _memcpy - copies memory area from source to memory of destination string
+ *
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: returns destination string
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+/**
+ * _realloc - reallocates a memory area with a new memory area
+ *
+ * @ptr: old pointer to a old memory area
+ * @old_size: size of the old pointer
+ * @new_size: size of the new memory area
+ *
+ * Return: returns a newly allocated memory area
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size <= old_size)
+	{
+		return (ptr);
+	}
+
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+
+	new_ptr = malloc(sizeof(int) * new_size);
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	free(ptr);
+	_memcpy(new_ptr, ptr, new_size);
+	return (new_ptr);
+}
