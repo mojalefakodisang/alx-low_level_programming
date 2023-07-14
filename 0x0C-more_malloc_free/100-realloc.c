5ab0d9bbe9f074a4f71926fcf820859b409195c8
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-14 06:14:31 -0700

    reallocates memory areas into a new memory area

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,21 +27,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
-
 	if (!ptr)
 	{
 		return (malloc(new_size));
 	}
-
 	new_ptr = malloc(new_size);
 
 	if (!new_ptr)
 	{
 		return (NULL);
 	}
-
 	old_ptr = ptr;
-
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
@@ -56,7 +52,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			new_ptr[i] = old_ptr[i];
 		}
 	}
-
 	free(ptr);
 	return (new_ptr);
 }
