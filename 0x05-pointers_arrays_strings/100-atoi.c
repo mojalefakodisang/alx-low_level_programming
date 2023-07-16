1215bb327339adc856b22b3553a9ac308edc44ea
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-16 15:14:28 -0700

    converts characters into integers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,18 +16,17 @@ int _atoi(char *s)
 	sign = 1;
 	result = 0;
 
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i = 1;
-	}
 	while (s[i] != '\0')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[i] != '\0')
+		{
+			sign = sign * -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			result = result * 10 + (s[i] - '0');
 		}
-		else
+		else if (result > 0)
 		{
 			return (0);
 		}
