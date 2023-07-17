8ad132d86b4edf57d2f9bd842ccdb97d5bb15ff6
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-17 04:05:57 -0700

    displays a file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#ifndef FILE
+#define FILE
+
 #define FILE __FILE__
 
+#endif
+
+/**
+ * main - prints the file name
+ *
+ * Return: returns 0 on success
+ */
 int main(void)
 {
 
