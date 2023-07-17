ba0b1c9720910c7a4be95b70fbcb887e37b7b0f4
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-17 10:55:04 -0700

    structure contain a dog data types

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - has variables for its name, age and its owner
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner name of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
