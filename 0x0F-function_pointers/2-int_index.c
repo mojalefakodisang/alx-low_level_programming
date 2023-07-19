2d607fa9422457c1c960a83fa8adf0c6724c43f6
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-19 08:25:48 -0700

    prints index of array if the comparison is true

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - function that prints index if comparison is true
+ *
+ * @array: array to be used
+ * @size: size of the array
+ * @cmp: function pointer to be used
+ *
+ * Return: returns index if successful, else -1 or 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		if (size <= 0 || !cmp)
+		{
+			return (-1);
+		}
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != NULL)
+			{
+				return (i);
+			}
+		}
+	}
+	return (0);
+}
