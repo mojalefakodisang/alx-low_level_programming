80fd269bfab9089e98ced0620db36fc31fce2f84
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-19 07:29:11 -0700

    prints an array of numbers depending on the function pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints an array of numbers depending on the
+ * function pointer action
+ *
+ * @array: array of numbers to be printed
+ * @size: size of the array
+ * @action: a pointer to the functio to be used
+ *
+ * Return: no return value
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (array != NULL)
+		{
+			action(array[i]);
+		}
+		i++;
+	}
+}
