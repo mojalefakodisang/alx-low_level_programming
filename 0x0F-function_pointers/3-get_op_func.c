e3d2131c4653eea5632d3230cdfd7eea259c7d02
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-19 13:37:37 -0700

    add mul element on the structure

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,6 +16,7 @@ int (*get_op_func(char *s))(int, int)
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
+		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
@@ -25,12 +26,12 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (*ops[i].op == *s && s[1] == '\0')
 		{
 			return (ops[i].f);
 		}
 		i++;
 	}
 
-	return (0);
+	return (NULL);
 }
