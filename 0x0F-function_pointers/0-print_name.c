0cd6caddd6044fcb99dbee9fd42dbfcc256fb44d
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-19 06:47:59 -0700

    prints a name depending on the function pointer passed on it

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name depending on the function pointer
+ *
+ * @name: name to be printed
+ * @f: function pointer to a specific function
+ *
+ * Return: no return value
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
