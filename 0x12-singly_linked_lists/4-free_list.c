f7fe8968996264f4b6b7f01e47f5538fc9432939
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-07-27 15:53:29 -0700

    frees allocated memory

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - frees memory allocated to the list
+ *
+ * @head: starting node of the list
+ *
+ * Return: no return value
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr = *head;
+	list_t *next;
+
+	while (ptr != NULL)
+	{
+		next = ptr->next;
+		free(ptr->str);
+		free(ptr);
+		ptr = next;
+	}
+	*head = NULL;
+}
