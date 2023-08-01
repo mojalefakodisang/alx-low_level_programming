02c6067103b10d1744115135b7f27944729b23c0
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-08-01 12:56:45 -0700

    fixed return to temp data

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,14 +10,16 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int data;
 
 	if (*head == NULL)
 	{
 		return (0);
 	}
 	temp = *head;
+	data = temp->n;
 	*head = (*head)->next;
 	free(temp);
 
-	return (1);
+	return (data);
 }
