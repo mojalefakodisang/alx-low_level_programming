18945b35acbb8edda0669b8fe85747b017cf36b4
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-08-01 04:32:28 -0700

    inserts a node at a given point of the list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
+	if (new_node->n == '\0')
+	{
+		return (NULL);
+	}
 	if (idx == 0)
 	{
 		new_node->next = *head;
