2fe0be9a509f82df4d723365b905e78f72d14cc9
Mojalefa Kodisang <105329271+mojalefakodisang@users.noreply.github.com>
2023-08-01 19:42:18 +0000

    deletes node at head

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a list
+ * 
+ * @head: first node of the list
+ * 
+ * Return:
+*/
+int pop_listint(listint_t **head)
+{
+	node *temp;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+
+	return (1);
+}
