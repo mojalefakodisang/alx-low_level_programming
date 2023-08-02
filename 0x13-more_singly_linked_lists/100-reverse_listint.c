45497fe328ba0c310dd1fe484931bcb6dda80745
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-08-02 13:07:24 -0700

    reduced required variables to two

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,14 +11,13 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
 	listint_t *current = *head;
-	listint_t *next = NULL;
 
 	while (current != NULL)
 	{
-		next = current->next;
+		*head = current->next;
 		current->next = prev;
 		prev = current;
-		current = next;
+		current = *head;
 	}
 
 	*head = prev;
