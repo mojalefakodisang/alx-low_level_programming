d95811bc3dc5694aeff442530f1f1530cef3740f
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-14 12:43:59 -0700

    [Task 3] - Adds a new node at the end of the list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,7 +25,7 @@ dlistint_t *add_dnodeint_end(dlistint_t  **head, const int n)
 	{
 		new_node->prev = NULL;
 		*head = new_node;
-		return (NULL);
+		return (new_node);
 	}
 
 	while (last->next != NULL)
