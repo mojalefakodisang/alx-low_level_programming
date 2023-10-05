d98d2bbd1c507827b470104f72b52dc213757469
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-10-05 10:34:17 -0700

    [Task 0] (Update) - Casted on memory allocations

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
-	hash_table_t *table = malloc(sizeof(hash_table_t *));
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t **));
+	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
