8a962c11bc336613882457cd2aeff1fab001efd9
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-12-21 13:51:54 -0800

    [Task 0] - Fixed the task + fixed README.md

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,10 +10,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i = 0;
 
 	table = malloc(sizeof(hash_table_t));
 	table->size = size;
-	table->array = NULL;
+	table->array = calloc(table->size, sizeof(hash_node_t **));
+
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
--- 0x1A-hash_tables/README.md
+++ 0x1A-hash_tables/README.md
@@ -1,1 +1,1 @@
-0x1A - Hash Tables
+# 0x1A - Hash Tables
