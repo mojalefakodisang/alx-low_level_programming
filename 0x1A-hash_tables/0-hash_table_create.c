259652a53d199cd327ff5b319d7cfa3b1ae7b3c2
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-12-21 13:59:00 -0800

    [Task 0] - Fixed the task + fixed README.md

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,8 +13,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i = 0;
 
 	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
 	table->size = size;
-	table->array =(hash_node_t **)calloc(table->size, sizeof(hash_node_t **));
+	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t **));
+
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
