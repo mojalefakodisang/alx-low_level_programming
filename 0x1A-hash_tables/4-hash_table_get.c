6daa8bffbdcced06da7b18b8d9aac1d4368cd8a0
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-12-27 06:06:03 -0800

    [Task 4] - Gets the value of a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ *
+ * @ht: table to get a value with a certain key
+ * @key: key to be found in the table
+ *
+ * Return: returns the value of the key if found else NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *curr;
+	const long int index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht == NULL || ht->size == 0 || key == NULL || index < 0)
+		return (NULL);
+
+	curr = ht->array[index];
+	while (curr != NULL)
+	{
+		if (strcmp(curr->key, key) == 0)
+			return (curr->value);
+		else
+			return (NULL);
+		curr = curr->next;
+	}
+	return (NULL);
+}
