988f049565b3c1536df584f56b0ab6add503670e
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-12-27 05:37:41 -0800

    [Update] - Fixed betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,16 +16,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || ht->size == 0 || key == NULL || value == NULL)
 		return (0);
-
 	new_item = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (new_item == NULL)
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
 	new_item->key = strdup(key);
 	new_item->next = NULL;
 	new_item->value = NULL;
-
 	if (value != NULL)
 		new_item->value = strdup(value);
 	if (ht->array[index] == NULL)
@@ -33,21 +30,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		curr = ht->array[index];
-
 		while (curr != NULL)
 		{
 			if (strcmp(curr->key, new_item->key) == 0)
 			{
 				free(curr->value);
 				curr->value = strdup(value);
-
 				free(new_item->key);
 				free(new_item->value);
 				free(new_item);
 
 				return (1);
 			}
-			
 			curr = curr->next;
 		}
 		new_item->next = ht->array[index];
