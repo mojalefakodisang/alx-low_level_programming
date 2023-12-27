6e095c16e6341a321146cab6f93b89f0004898b9
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-12-27 06:27:47 -0800

    [Task 4] - Updated the logics

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *curr;
-	const long int index;
+	unsigned long int index;
 
 	if (ht == NULL || ht->size == 0 || key == NULL || strcmp(key, "") == 0)
 		return (NULL);
