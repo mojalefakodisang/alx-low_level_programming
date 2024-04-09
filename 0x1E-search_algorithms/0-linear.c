7b0aec17397316bf655cc34800ecadd51086b193
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-04-09 23:05:04 +0200

    [Update] - Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,7 +13,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size < 1)
 		return (-1);
 
 	for (i = 0; i < size; i++)
